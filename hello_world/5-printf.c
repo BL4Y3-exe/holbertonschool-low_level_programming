c37db33beaf585c87d463dd95d7f7fd24305fda6
Ilknur <ilknurkazimov308@gmail.com>
2025-11-28 12:36:38 +0000

    Add 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - entry point
  *
- * return = 0
+ * return: always 0
  */
 int main()
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return = 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
