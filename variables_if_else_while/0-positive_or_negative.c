07b2966b1ecde74cd44f6f68e4165e1d2434c060
Ilknur <ilknurkazimov308@gmail.com>
2025-12-01 12:23:21 +0000

    Add 0-positive_or_negative.c code

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ * /
 int main(void)
 {
 	int n;
