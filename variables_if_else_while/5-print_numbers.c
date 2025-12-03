d1398cdb201d23b0aa24c956ccbab7c9d9d4c5d4
Ilknur <ilknurkazimov308@gmail.com>
2025-12-03 09:54:45 +0000

    Add 5-print_numbers.c code

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ *
+ * return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for(c = "0"; c <= "9"; c++);
+	putchar(c);
+
+	return (0);
+}
