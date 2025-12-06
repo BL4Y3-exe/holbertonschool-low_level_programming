f2ef39041fdb13ad552ce27f9f3f32b30282bbce
Ilknur <ilknurkazimov308@gmail.com>
2025-12-06 12:52:19 +0000

    add 1-alphabet.c code

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints alpahabet in lowercas
+*
+* Return: void
+*/
+void print_alphabet(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+        _putchar(c);
+
+    _putchar('\n')
+}
