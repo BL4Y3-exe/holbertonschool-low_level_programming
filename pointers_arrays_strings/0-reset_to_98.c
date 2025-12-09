e7e9aa3736eba5fe93332246084ad4f4eb87a381
Ilknur <ilknurkazimov308@gmail.com>
2025-12-09 11:38:27 +0000

    add 0-reset_to_98.c code

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
-* reset_to_98 - updates the value of int to 98
-* @n: pointer to an int
-*
-* Return: void
-*/
+ * reset_to_98 - updates the value of the int it points to to 98
+ * @n: pointer to an int
+ */
 void reset_to_98(int *n)
 {
-    *n = 98;
-    
+	*n = 98;
 }
