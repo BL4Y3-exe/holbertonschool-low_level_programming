e73bb427503ca9cceb1a394c9d95d0a79dd1e8dc
Ilknur <ilknurkazimov308@gmail.com>
2025-12-09 13:26:52 +0000

    add 7-puts_half.c code

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,11 @@ void puts_half(char *str)
 	int len = 0;
 	int i;
 
-	/* Считаем длину строки */
 	while (str[len] != '\0')
 		len++;
 
-	/* Если длина нечётная, начинаем с (len + 1) / 2 */
 	i = (len % 2 == 0) ? (len / 2) : ((len + 1) / 2);
 
-	/* Печатаем вторую половину */
 	while (i < len)
 	{
 		_putchar(str[i]);
