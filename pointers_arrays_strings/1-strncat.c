90e33af2d00128d434588628cb7225f841ca7769
Ilknur <ilknurkazimov308@gmail.com>
2025-12-10 13:13:20 +0000

    Add 1-srtncat.c cpde

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,31 @@
 #include "main.h"
 
+/**
+ * _strncat - concatenates two strings using at most n bytes from src
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes to copy from src
+ *
+ * Return: dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-    while (src[j] != 0 && j < n)
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	while (src[j] != 0 && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    dest[i] = '\0';
+	dest[i] = '\0';
 
-    return (dest);
+	return (dest);
 }
