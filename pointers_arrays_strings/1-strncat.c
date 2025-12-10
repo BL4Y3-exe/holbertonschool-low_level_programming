61fadfb3314811ef21eaf85029c5f8a275ad9193
Ilknur <ilknurkazimov308@gmail.com>
2025-12-10 13:11:26 +0000

    Add 1-strncat.c code

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 
     while (src[j] != 0 && j < n)
     {
-        dest[i] = src[j]
+        dest[i] = src[j];
         i++;
         j++;
     }
 
-    dest[i] = '\0'
+    dest[i] = '\0';
 
     return (dest);
 }
