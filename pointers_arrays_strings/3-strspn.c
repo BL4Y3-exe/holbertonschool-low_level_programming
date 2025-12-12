c02e0ea2be2c872ec6ff15ee62d1105ccebb1026
Ilknur <ilknurkazimov308@gmail.com>
2025-12-12 12:12:06 +0000

    Add 3-strspn.c code

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned i; j; count;
+
+    count = 0;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                count++;
+                break;
+            }
+        }
+
+        if (accept[j] == '\0')
+            return (count);
+    }
+
+    return (count);
+}
