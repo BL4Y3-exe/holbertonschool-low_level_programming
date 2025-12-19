288c1d53b1a0ffeed35cae80cae0c3141b91620a
Ilknur <ilknurkazimov308@gmail.com>
2025-12-19 09:21:59 +0000

    Add 5-sqrt_recursion.c code

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* helper - helper func to find square root
+* @n: the number whose square root needs to be found
+* @prbl: current probable value of square root
+*
+* Return: square root or -1
+*/
+int helper(int n, int prbl)
+{
+    if (prbl * prbl == n)
+        return (prbl);
+
+    if (prbl * prbl > n)
+        return (-1);
+
+    return (helper (n, prbl + 1))
+}
+
+/**
+* _sqrt_recursion - returns the natural square root of number
+* @n: the number whose square root needs to be found
+*
+* Return: natural square root or -1 if not exist*/
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+        return (-1);
+    
+    return (helper(n, 0));
+}
