225c1a1d216c2e71666d4213ea6a79639bb27f29
Ilknur <ilknurlazimov308@gmail.com>
2025-12-19 12:12:02 +0000

    Add 3-mul.c code

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
     int a, b;
     a = atoi(argv[1]);
     b = atoi(argv[2]);
-    if (argc == 2)
+    if (argc == 3)
     {
         printf("%d\n", a * b);
         return (0);
     }
 
     printf("Error\n");
-    
+
 	return (1);
 }
