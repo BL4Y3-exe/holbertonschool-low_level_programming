c1dc8df7e370a258f7d161ac5d51e20231d6a607
Ilknur <ilknurkazimov308@gmail.com>
2025-12-25 11:27:06 +0000

    Add 1-string_nconcat.c code

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,37 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to take from s2
+ *
+ * Return: pointer to newly allocated string, or NULL on failure
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char * new;
-    unsigned int i, j, len1 = 0, len2 = 0;
+	char *new;
+	unsigned int i, j, len1 = 0, len2 = 0;
 
-    if (s1 == NULL)
-        s1 = "";
-    
-    if (s2 == NULL)
-        s2 = "";
-    
-    while (s1[len1])
-        len1++;
-    
-    while (s2[len2])
-        len2++
-    
-    if (n >= len2)
-        n = len2;
-    
-    new = malloc(sizeof(char) * (len1 + n + 1));
-    if (new == NULL)
-        return (NULL);
+	if (s1 == NULL)
+		s1 = "";
 
-    for (i = 0; i < len1; i++)
-        new[i] = s1[i];
-    
-    for (j = 0; j < n; j++)
-        new[i + j] = s2[j];
-    
-    new[i + j] = '\0';
+	if (s2 == NULL)
+		s2 = "";
 
-    return (new);
+	while (s1[len1])
+		len1++;
+
+	while (s2[len2])
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+
+	new = malloc(sizeof(char) * (len1 + n + 1));
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		new[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		new[i + j] = s2[j];
+
+	new[i + j] = '\0';
+
+	return (new);
 }
