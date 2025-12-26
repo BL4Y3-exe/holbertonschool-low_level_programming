603a732bdd902c9c94eb06cbbf0a50bbea651afb
Ilknur <ilknurkazimov308@gmail.com>
2025-12-26 07:15:00 +0000

    Add 2-calloc.c code

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    void *pCalloc;
+    unsigned int i;
+    char *c;
+
+    if (nmemb == 0 || size == 0)
+        return (NULL);
+    
+    pCalloc = malloc(nmemb * size)
+    if (pCalloc == NULL)
+        return (NULL);
+    
+    c = pCalloc;
+    for (i = 0; i < nmemb * size; i++)
+        c[i] = 0;
+    
+    return (pCalloc);
+}
