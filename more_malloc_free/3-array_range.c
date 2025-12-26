ea06e2be0ffa05eeac050fac4f18dbede0ecbe00
Ilknur <ilknurkazimov308@gmail.com>
2025-12-26 07:33:57 +0000

    Add 3-array_range.c code

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+int *array_range(int min, int max)
+{
+    int *pArr;
+    int i, size;
+
+    if (min > max)
+        return (NULL);
+
+    size = max - min + 1;
+
+    pArr = malloc(sizeof(int) * size)
+    if (pArr == NULL)
+        return (NULL);
+
+    for (i = 0; i < size; i++)
+        pArr[i] = min + i;
+
+    return (pArr);
+}
