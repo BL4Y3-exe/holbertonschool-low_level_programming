b7f23a9effc2bd384b47768fc9df1e509032a191
Ilknur <ilknurkazimov308@gmail.com>
2026-01-06 13:22:44 +0000

    Add 0-sum_them_all.c code

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,7 +13,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
- 	if (n == 0)
+	if (n == 0)
 		return (0);
 
 	va_start(args, n);
@@ -21,6 +21,7 @@ int sum_them_all(const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		int x = va_arg(args, int);
+
 		s += x;
 	}
 	va_end(args);
