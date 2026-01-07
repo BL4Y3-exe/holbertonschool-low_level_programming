2266f895ad1599ceb7c69de84da0c093163dc1ef
Ilknur <ilknurkazimov308@gmail.com>
2026-01-07 10:01:29 +0000

    Add 3-print_all code

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,12 +12,12 @@ void print_int(va_list args)
 	printf("%d", va_arg(args, int));
 }
 
-void print_float(va_list args);
+void print_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 }
 
-void print_string(va_list args);
+void print_string(va_list args)
 {
 	char *str;
 
