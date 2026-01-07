dee726243800ddb31ec21de3ddbe6be9804caf3d
Ilknur <ilknurkazimov308@gmail.com>
2026-01-07 08:55:24 +0000

    Add 2-print_strings.c code

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+* print_strings - function that prints strings, followed by a new line
+* @separator: string to be printed between the strings
+* @n: number of strings passed to the function
+*/
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
