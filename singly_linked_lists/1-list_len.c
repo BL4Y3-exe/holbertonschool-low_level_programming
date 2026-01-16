1880293d273cc18d4bc74403d9dd73a106a0c298
Ilknur <ilknurkazimov308@gmail.com>
2026-01-16 10:23:23 +0000

    Add 1-list_len.c code

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* list_len - function that returns the number of elements in a linked list_t list
+* list_len - returns the number of elements in a linked list_t list
 * @h: pointer to the head of list
 *
 * Return: number of elements
