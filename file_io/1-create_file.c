52c9666c73d0ac8955dd19fa7160a767d487d01e
Ilknur <ilknurkazimov308@gmail.com>
2026-01-25 10:53:14 +0000

    Add 1-create_file.c code

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes_written;
 	size_t len = 0;
 
-	if (filename == NULL);
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
