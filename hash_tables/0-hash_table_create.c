feae28638c12ddf220b5ba37aa3bbaeb4cf2ad40
Ilknur <ilknurkazimov308@gmail.com>
2026-01-25 17:32:47 +0000

    Add 0-hash_table_create.c code

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
-* hash_table_create - creates a hash table\
-* @size: the size of the array
-*
-* Return: a pointer to the newly created hash table
-*/
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table, or NULL on failure
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
